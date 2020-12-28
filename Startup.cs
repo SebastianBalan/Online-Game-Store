22c3399044ddd2bf77d72178d846e184eb19d71f
Sebastian Balan <sebastian.balan99@gmail.com>
2020-12-28 16:07:51 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Game_store.Startup))]
+namespace Online_Game_store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
